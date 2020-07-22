ee80fe8590a3e18ec1fac7cd10cdce6efa52c0c6
UberStash <expiredserver@gmail.com>
2020-07-22 23:09:41 +0000

    added assertequal to letterposition and added unfinished findkeybyvalue

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,19 @@
 
+const assertArrayEqual = function(a, b) {
+  if (typeof a !== typeof b || eqArrays(a, b) === false) {
+    console.log(`🤯🤯🤯 Assertion Failed: ${a} !== ${b}`);
+  } else if (typeof a === typeof b && eqArrays(a, b) === true) {
+    console.log(`👍👍👍 Assertion Passed: ${a} === ${b}`);
+  }
+};
 
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.toString() === arrTwo.toString()) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 
 const letterPositions = function(sentence) {
@@ -11,12 +25,12 @@ const letterPositions = function(sentence) {
    
     if (results[sentence[i]]) {
       results[sentence[i]].push(i);
-      console.log('hit if');
+      //console.log('hit if');
     } else if (sentence[i] === ' ') {
       continue;
     } else {
       results[sentence[i]] = [i];
-      console.log('hit else');
+      //console.log('hit else');
     }
   }
     
@@ -26,4 +40,5 @@ const letterPositions = function(sentence) {
   return results;
 };
 
+assertArrayEqual(letterPositions("hello").e, [1]);
 console.log(letterPositions("lighthouse in the house"));
