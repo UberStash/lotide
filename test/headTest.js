a61e591c2248cc10ee19259586b6b7533561e74c
UberStash <expiredserver@gmail.com>
2020-07-27 20:08:30 +0000

    completed headTest mocha chai refactor

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,23 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-// head test code
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'hello' for ['hello']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+  it("returns 'hello' for ['hello']", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+});
